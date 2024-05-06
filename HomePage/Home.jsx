3434ca502fa4c5d6d249e5837ff2ceb0768c742a
Chanmala KeoxaisyCom2 <chanmalakeoxaisy@gmail.com>
2024-05-06 16:16:11 +0700

    Home Page submit (not success)

---
+++ HomePage/Home.jsx
@@ -0,0 +1,16 @@
+import './Style.css';
+import { IoPawSharp } from "react-icons/io5";
+const Home = () =>{
+  return (
+    <div>
+      <h1>AtoonPortfolio</h1>
+      <IoPawSharp/>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Content</li>
+      </ul>
+    </div>
+  );
+};
+export default Home;
