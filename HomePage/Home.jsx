bb6332280c2b3158fb31643a54ffd8ce5f9f363b
Chanmala KeoxaisyCom2 <chanmalakeoxaisy@gmail.com>
2024-05-09 09:55:34 +0700

    next the element style of awarded component

--- HomePage/Home.jsx
+++ HomePage/Home.jsx
@@ -1,44 +1,100 @@
+import { useState } from "react";
+import data from "./Data";
 import "./Style.css";
 // import { PiDogDuotone } from "react-icons/pi";
 import { SiProbot } from "react-icons/si";
 import { IoPawSharp } from "react-icons/io5";
+import { PiPawPrintBold } from "react-icons/pi";
 const Home = () => {
+  const [selection, setSelection] = useState(null);
+
+  const SelectiononClick = (getSelection) => {
+    setSelection(getSelection === selection ? null : getSelection);
+  };
+  console.log(selection);
   return (
-    <div className="mx-auto w-full h-auto px-4 ">
-      <div className="bg-white sticky top-0 h-17">
+    <div className="mx-auto w-full h-auto px-4 bg-white">
+      <div className="bg-white sticky top-0 h-17 mx-11">
         <div className="flex justify-between items-center">
-          <h1 className="font-semibold text-xl text-blue-950 mt-2">AtoonPortfolio</h1>
+          <div className="flex">
+            <h1 className="font-semibold text-2xl text-gray-900 mt-2">Atoon</h1>
+            <p className="mt-2 font-semibold text-2xl text-rose-400">
+              Portfolio
+            </p>
+          </div>
           {/* <PiDogDuotone size="30px"/> */}
-          <p className="px-3 py-3 mt-3 bg-blue-500 rounded-full"></p>
+          <IoPawSharp size="25px" className="mt-3 text-rose-500" />
           <ul className="flex justify-end space-x-4">
-            <li className="font-semibold text-base text-gray-600 pt-3">Home</li>
-            <li className="font-semibold text-base text-gray-600 pt-3">About</li>
-            <li className="font-semibold text-base text-gray-600 pt-3">
+            <li className="font-semibold text-base text-gray-600 pt-4">Home</li>
+            <li className="font-semibold text-base text-gray-600 pt-4">
+              About
+            </li>
+            <li className="font-semibold text-base text-gray-600 pt-4">
               Content
             </li>
           </ul>
         </div>
-        <hr className="border-1 mt-8 shadow-md text-gray-500"/>
+        <hr className="border-1 mt-7 shadow text-rose-200 border-rose-100" />
       </div>
 
-      <div className="flex justify-center mt-24 mx-11">
-        <div>
-          <img src="https://i.pinimg.com/564x/2f/c9/83/2fc983dc27f30414efa1b37153092066.jpg" alt="#" className="w-3/4 h-3/4 ml-10"/>
-        </div>
-        <div className="mt-14">
-          <p className="font-semibold text-3xl text-gray-900 mr-2 ">Hi! My name Chanmala</p>
-          <div className="flex space-x-2">
-            <p className="font-medium text-xl text-gray-700 pt-1">Nickname: Atoon </p>
-              <IoPawSharp className="mt-2 h-6 w-5 text-rose-500"/>
+      <div className="flex justify-center mt-24">
+        <div className="text-center">
+          <p className="font-semibold text-3xl text-gray-900 mr-2 ">
+            Hi! My name AtoonChanmala
+          </p>
+          <p className="font-medium text-lg text-gray-600 pt-1 md:2/3 ">
+            Faculty of Engineering,Computer Engineering & IT | october 2021 -
+            PRESENT
+          </p>
+          <div className="flex justify-center space-x-1">
+            <p className="text-sm py-1 text-gray-500">Member of CEIT ROBOTIC</p>
+            <SiProbot className="mt-1 text-gray-500" />
+            <p className="text-sm py-1 text-gray-500"> | 2022 - PRESENT</p>
           </div>
-          <div className="flex space-x-1">
-            <p className="text-sm py-1 text-gray-500">CEIT ROBOTIC</p>
-            <SiProbot className="mt-1 text-gray-500"/>
-          </div>
-            <button className="mt-1 border-3 rounded-md border-rose-400 bg-blue-600 text-gray-200 font-semibold px-2 py-2 text-center">Im ENTJ</button>
         </div>
+      </div>
+
+      <div className="mt-28 text-center flex justify-center space-x-1">
+        <h1 className="font-semibold text-2xl text-gray-900">AWARDED &</h1>
+        <h1 className="font-semibold text-2xl text-blue-500">SPONSORSHIP</h1>
+      </div>
+      <p className="text-center text-sm text-gray-500 font-base ml-3 pt-1">
+        Click the Icon paw to see Awarded
+      </p>
+
+      <div className="flex mt-5 w-full flex-wrap mx-11">
+        {data && data.length ? (
+          data.map((dataItem) => (
+            <div
+              key={dataItem.id}
+              className="flex flex-row w-1/3 justify-center"
+            >
+              <div className="w-1/2">
+                <div
+                  onClick={() => SelectiononClick(dataItem.id)}
+                  className="flex space-x-1 mt-5 border-2 border-rose-400 rounded-md px-4 py-2 "
+                >
+                  <h3 className="font-semibold text-lg items-center">{dataItem.question}</h3>
+                  <PiPawPrintBold size="20px" className="mt-1" />
+                </div>
+
+                {selection === dataItem.id ? (
+                  <div className="">
+                    <h3>{dataItem.answer}</h3>
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          ))
+        ) : (
+          <div>
+            <h3>Data not fond!!</h3>
+          </div>
+        )}
       </div>
     </div>
   );
 };
 export default Home;
+
+        // tor jut items
