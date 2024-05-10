9076bb2ee29ddceb5858360331f6bae9e4541f5c
Chanmala KeoxaisyCom2 <chanmalakeoxaisy@gmail.com>
2024-05-10 09:16:38 +0700

    Data for awarded and sponsorship component

---
+++ HomePage/Data.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        id:"1",
+        question:"5th year primary education",
+        answer:"Participated in the Outstanding Student Competition in Mathematics | 2011 - 2012"
+    },
+    {
+        id:"2",
+        question:"4th year secondary education",
+        answer:"Participated in the Outstanding Student Competition in Chemistry | 2015 - 2016"
+    },
+    {
+        id:"3",
+        question:"7th year secondary education",
+        answer:"Participated in the Outstanding Student Competition in Chemistry | 2020 - 2021"
+    },
+    {
+        id:"4",
+        question:"Youth Innovation Callenge",
+        answer:"TOMI AI (1st runner-up) | 23.12.2023"
+    },
+    {
+        id:"5",
+        question:"Fina Finance E-Commerce",
+        answer:"ShopDown Application (2nd runner-up) | 20.03.2023"
+    }
+]
+export default data;
